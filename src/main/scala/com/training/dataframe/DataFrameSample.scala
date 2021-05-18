2030433ce0cc915b5a036f1130f4b6c79dadc405
sujith <sujithmohanan@gmail.com>
2021-05-18 10:20:05 +0530

    DataFrame Sample program

--- src/main/scala/com/training/dataframe/DataFrameSample.scala
+++ src/main/scala/com/training/dataframe/DataFrameSample.scala
@@ -1,5 +1,22 @@
 package com.training.dataframe
 
+import org.apache.spark.sql.SparkSession
+
 object DataFrameSample {
 
+  def main(args: Array[String]): Unit = {
+    val sparkSession = SparkSession.builder()
+      .appName("DataFrameTest")
+      .master("local[2]")
+      .getOrCreate()
+
+    val file = "/home/sujith/Downloads/baseball.csv"
+
+    val df = sparkSession.read
+      //.option("quote","\"")
+      //.option("escape","\"")
+      .option("header", true)
+      .csv(file)
+    df.show()
+  }
 }
