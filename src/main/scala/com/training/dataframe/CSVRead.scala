95cf2e63f15f335341d3f4457821ade9922f8d9c
sujith <sujithmohanan@gmail.com>
2021-05-23 20:43:10 +0530

    DataFrame json program

--- src/main/scala/com/training/dataframe/CSVRead.scala
+++ src/main/scala/com/training/dataframe/CSVRead.scala
@@ -5,36 +5,52 @@ import org.apache.spark.sql.types.{IntegerType, StringType, StructField, StructT
 
 object CSVRead {
 
+  //creating SparkSession
   val sparkSession = SparkSession.builder()
     .appName("CSVRead")
     .master("local[2]")
     .getOrCreate()
 
+  ///Main method
   def main(args: Array[String]): Unit = {
+    /**
+     * Calling 2 methods
+     *    dynamicSchema := Spark reads schema by its own
+     *                    (But a task will get created to infer schema)
+     *    staticSchema  := We will provide schema.
+     *                    (So that spark can directly go to process data.)
+     */
     dynamicSchema
     staticSchema
+
+    //close sparksession
+    sparkSession.close()
   }
 
+  //method := dynamicSchema
   def dynamicSchema = {
     noQuotes("/home/sujith/Downloads/baseball.csv").show()
     withQuotes("/home/sujith/Downloads/customer.csv").show()
     fewQuotes("/home/sujith/Downloads/customer_attendance.csv").show()
   }
 
+  //method to process csv without quotes
   def noQuotes(file: String) = {
     sparkSession.read
       .option("header", true)
       .csv(file)
   }
 
+  //method to process csv with quotes
   def withQuotes(file: String) = {
     sparkSession.read
-      .option("quote", "\"")
-      .option("ignoreLeadingWhiteSpace", true)
-      .option("header", true)
+      .option("quote", "\"")  // to specify quote
+      .option("ignoreLeadingWhiteSpace", true) //will ignore leading white spaces for cols
+      .option("header", true) //header flag
       .csv(file)
   }
 
+  //method to process csv having both quoted and normal cols
   def fewQuotes(file: String) = {
     sparkSession.read
       .option("quote", "\"")
@@ -43,11 +59,13 @@ object CSVRead {
       .csv(file)
   }
 
+  //method := staticSchema
   def staticSchema = {
     withStruct("/home/sujith/Downloads/customer_attendance.csv").show()
     withDDL("/home/sujith/Downloads/customer_attendance.csv").show()
   }
 
+  //method proving schema with StrutType
   def withStruct(file: String) = {
     val schema = StructType(Array(StructField("Name", StringType, false),
       StructField("Dept", StringType, false),
@@ -61,6 +79,7 @@ object CSVRead {
 
   }
 
+  //method proving schema with DDL
   def withDDL(file: String) = {
     val schema = "Name STRING, Dept STRING, Attendance INT"
     sparkSession.read
--- src/main/scala/com/training/dataframe/DataFrameSample.scala
+++ src/main/scala/com/training/dataframe/DataFrameSample.scala
@@ -5,16 +5,25 @@ import org.apache.spark.sql.SparkSession
 object DataFrameSample {
 
   def main(args: Array[String]): Unit = {
+
+    ///creating spark session
     val sparkSession = SparkSession.builder()
       .appName("DataFrameTest")
       .master("local[2]")
       .getOrCreate()
 
+    //inputfile
     val file = "/home/sujith/Downloads/baseball.csv"
 
+    //reading file
     val df = sparkSession.read
       .option("header", true)
       .csv(file)
+
+    //display contents
     df.show()
+
+    //closing spark session
+    sparkSession.close()
   }
 }
